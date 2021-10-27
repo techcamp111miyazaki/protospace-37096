372bc21c93ab9eb6e913889c6f8406ddd0f636e5
techcamp111miyazaki <kirohi19980715@gmail.com>
2021-10-28 08:50:17 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,62 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :move_to_index, only: [:edit]
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      @prototype = Prototype.new(prototype_params)
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    @prototype.update_attributes(prototype_params)
+    if @prototype.save
+      redirect_to prototype_path(@prototype.id)
+    else
+      @prototype.attributes = (prototype_params)
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private##
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == prototype.user.id
+      redirect_to action: :index
+    end
+  end
+
+end
